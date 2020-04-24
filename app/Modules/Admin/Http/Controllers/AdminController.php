28aaf9b8fc9a0b244e7160a477628d3038215c3c
Monika <58719795+monikamourya91@users.noreply.github.com>
2020-04-24 18:37:23 +0530

    admin

---
+++ app/Modules/Admin/Http/Controllers/AdminController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Modules\Admin\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class AdminController extends Controller
+{
+	/**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth:admin');
+    }
+
+
+
+    /**
+     * Display the module welcome screen
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function welcome()
+    {
+        return view("Admin::welcome");
+    }
+
+    public function dashboard()
+    {
+        return view("Admin::dashboard");
+    }
+
+}
+
---
+++ app/Modules/Admin/routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+Route::get('admin/dashboard', 'AdminController@dashboard');
+
+
+Route::get('admin/login', 'Auth\LoginController@showLoginForm')->name('admin.login');
+
+Route::post('admin/login', 'Auth\LoginController@login');
+
+Route::post('admin/logout', 'Auth\LoginController@logout');
+
+//Auth::routes();
+Route::get('admin/register', 'Admin\Auth\RegisterController@showRegistrationForm');
+
+Route::post('admin/register', 'Auth\RegisterController@register');
+
+
+
+
+
