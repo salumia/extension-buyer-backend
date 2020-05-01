9bdf80dc88e7702625da2d78dd91108f12b9bdee
Monika <58719795+monikamourya91@users.noreply.github.com>
2020-05-01 13:11:10 +0530

    category

---
+++ app/Modules/Admin/Http/Controllers/CategoryController.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace App\Modules\Admin\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Modules\Admin\Models\Category as Category;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Hash;
+use Session;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        if (isset($_GET['category'])){
+            dd('here');
+          $categories = Category::Where('category_name', 'like', '%' . $_GET['category'] . '%')->orderBy('id', 'DESC')->get();
+        }else{
+            $categories = Category::all();
+        } 
+        return view('Admin::categories.categories',compact('categories'));
+        
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('Admin::categories.add_categories');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [ 
+            'categoryName' => 'required',
+            'status' => 'required',
+        ]);
+        if ($validator->fails()) { 
+            return response()->json(['error'=>$validator->errors()], 200);            
+        }
+        $category= new Category;
+        $category->category_name=$request->categoryName;
+        $category->status=$request->status;
+        $category->Save();
+        return redirect('/admin/categories')->with('success','Category Saved Successfully.');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $category=Category::find($id);
+        return view('Admin::categories.edit_categories',compact('category'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $validator = Validator::make($request->all(), [ 
+            'categoryName' => 'required',
+            'status' => 'required',
+        ]);
+        if ($validator->fails()) { 
+            return response()->json(['error'=>$validator->errors()], 200);            
+        }
+        $category= Category::find($id);
+        $category->category_name=$request->categoryName;
+        $category->status=$request->status;
+        $category->Update();
+        return redirect('/admin/categories')->with('success','Category Updated Successfully.');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $category= Category::find($id);
+        $category->Delete();
+        return redirect('/admin/categories')->with('success','Category Delete Successfully.');
+    }
+}
