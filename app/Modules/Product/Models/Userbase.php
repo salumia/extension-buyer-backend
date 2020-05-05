cedb693108bbc7ad28d0cc8f65627b87ec4cdec3
Monika <58719795+monikamourya91@users.noreply.github.com>
2020-05-05 11:07:40 +0530

    offer model

---
+++ app/Modules/Product/Models/Userbase.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Modules\Product\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Userbase extends Model
+{
+    
+     protected $fillable = [
+       "country_id","product_id","users"
+    ];
+    
+}
